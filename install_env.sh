56b84d41658b5f2d7878d116d63aabc7e9fde37e
dr-coton <dr.coton@gmail.com>
2020-11-18 19:45:32 +0900

    update

--- install_env.sh
+++ install_env.sh
@@ -2,8 +2,8 @@
 
 apt update && apt upgrade -y
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-apt install -y nodejs
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 apt install npm -y
 apt install unzip -y
